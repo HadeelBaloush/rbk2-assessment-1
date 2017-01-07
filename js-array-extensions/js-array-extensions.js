fea2ae7ddc3a5d9e0d4db330be31998e4e823b2f
hadeel adham <hadeel adham>
2017-01-07 12:09:42 +0400

    Array ext.

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,13 @@
 // your code here
+
+
+Array.prototype.first = function() {
+	// body...
+	return this[0];
+};
+
+
+Array.prototype.last = function() {
+	// body...
+	return this[this.length - 1];
+};
